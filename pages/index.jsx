ac50c3f713d49ff67a6a00bf5fa821e297a004fe
Gold7EXE <156623102+Gold7EXE@users.noreply.github.com>
2024-02-16 22:35:08 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -86,7 +86,7 @@ Vote Dove</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://cdn.discordapp.com/attachments/1178530750144970924/1208254933082701935/image.png?ex=65e29e18&is=65d02918&hm=444c1b3194be824f21b04d940ceea4d691b4bd16601f9254fa97825cc40359a3&"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Dove, the best and most up-to-date bot?</p>
                 </div>
